3362f9647155e54f1bed8984ed2b2413da1fd22e
nickstech <otienon046@gmail.com>
2024-03-06 14:05:38 +0300

    Updated

---
+++ tests/EmailTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+require "src/Email.php";
+
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmail(): void
+    {
+        $string = 'user@example.com';
+
+        $email = Email::fromString($string);
+
+        $this->assertSame($string, $email->asString());
+    }
+
+    public function testCannotBeCreatedFromInvalidEmail(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+    }
+
+    public function testCannotEmailCannotBeBlank(): void
+    {
+        $string = 'user@example.com';
+
+        $email = Email::fromString($string);
+
+        $this->assertSame($string, $email->asString());
+    }
+}
